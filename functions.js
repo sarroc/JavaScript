eaf225cf912b4a1caa1152c8f0e516d31acc5430
Sarah Rocco <sarahrocco90@gmail.com>
2025-01-01 18:41:28 -0500

    Work more on JavaScript functions

--- functions.js
+++ functions.js
@@ -107,3 +107,82 @@
 //}
 
 //AVERAGE
+// function avg(arr) {
+//   let total = 0;
+//loop over each number
+//   for (let num of arr) {
+// add them together
+// total += num;
+//   }
+// divide by number of nums
+//   return total / arr.length;
+// }
+//
+
+//Pangram (sentence using every letter of a given alphabet at least once)
+//Waltz, bad nymph, for quick jigs vex.
+//Sphinx of black quartz, judge my vow.
+//How quickly daft jumping zebras vex!
+//The five boxing wizards jump quickly.
+//Pack my box with five dozen liquor jugs.
+
+//function isPangram(phrase) {
+//let lowerCased = phrase.toLowerCase(); //so string can be uppercase
+//loop over each letter of the alphabet
+//for (let letter of "abcdefghijklmnopqrstuvwxyz") {
+//if (lowerCased.indexOf(letter) === -1) {
+//return false;
+//}
+//}
+//return true;
+//}
+
+// Playing Cards
+// function getCard() {
+//   const values = [
+// "2",
+// "3",
+// "4",
+// "5",
+// "6",
+// "7",
+// "8",
+// "9",
+// "10",
+// "J",
+// "Q",
+// "K",
+// "A",
+//   ];
+//   const valIdx = Math.floor(Math.random() * values.length); //decimel from 0-1 not including 10. math.floor gets rid of decimel.
+//   const value = values[valIdx];
+//   const suits = ["clubs", "spades", "hearts", "diamonds"];
+//   const suitIdx = Math.floor(Math.random() * suits.length);
+//   const suit = suits[suitIdx];
+//   console.log(value, suit);
+// }
+
+// function pick(arr) { //easier way to pick multiple values instead of repeating code.
+//   const idx = Math.floor(Math.random() * arr.length);
+//   return arr[idx];
+// }
+// function getCard() {
+//   const values = [
+    // "2",
+    // "3",
+    // "4",
+    // "5",
+    // "6",
+    // "7",
+    // "8",
+    // "9",
+    // "10",
+    // "J",
+    // "Q",
+    // "K",
+    // "A",
+//   ];
+//   const suits = ["clubs", "spades", "hearts", "diamonds"];
+//   return { value: pick(values), suit: pick(suits) };
+// }
+
